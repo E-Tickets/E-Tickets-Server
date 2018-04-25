6a20d5b4feed85eb1cd8d539be7dc18a056f46f2
qyb225 <qyb225@gmail.com>
2018-04-25 19:55:52 +0800

    [Config] Config port and database

---
+++ server/config/index.js
@@ -0,0 +1,12 @@
+const config = {
+    port: 8080,
+    database: {
+        DATABASE: 'e_tickets',
+        USERNAME: 'tickets_admin',
+        PASSWORD: 'tickets',
+        HOST: 'localhost',
+        PORT: 3306
+    }
+};
+
+module.exports = config;
