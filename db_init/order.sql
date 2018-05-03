69dfe5af63a15c403a7e4e78c61bd86918a80291
qyb225 <qyb225@gmail.com>
2018-05-03 14:46:20 +0800

    [Database] delete unique key in order, change tablename

--- db_init/order.sql
+++ db_init/order.sql
@@ -5,16 +5,16 @@
 
 use `e_tickets`;
 
-DROP TABLE IF EXISTS `order`;
+DROP TABLE IF EXISTS `ticket_order`;
 
-CREATE TABLE `order` (
+CREATE TABLE `ticket_order` (
     `order_id` int NOT NULL AUTO_INCREMENT,
     `username` varchar(64) NOT NULL,
     `schedule_id` int NOT NULL,
     `seat` int NOT NULL,
+    `is_paid` boolean NOT NULL DEFAULT false,
     
     PRIMARY KEY (`order_id`),
     FOREIGN KEY (`username`) REFERENCES `user`(`username`),
-    FOREIGN KEY (`schedule_id`) REFERENCES `movie_schedule`(`schedule_id`),
-    UNIQUE KEY (`username`, `schedule_id`)
+    FOREIGN KEY (`schedule_id`) REFERENCES `movie_schedule`(`schedule_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
