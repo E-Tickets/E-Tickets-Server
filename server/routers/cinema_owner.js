b1ebd9fa11abdb32e1f5415ac45fbec047933355
qyb225 <qyb225@gmail.com>
2018-06-30 01:08:19 +0800

    [user][admin][owner] get self routes

--- server/routers/cinema_owner.js
+++ server/routers/cinema_owner.js
@@ -4,4 +4,6 @@ const ownerController = require('../controllers/cinema_owner');
 
 router.post('/', ownerController.register);
 
+router.get('/self', ownerController.getOwnerSelf);
+
 module.exports = router;
